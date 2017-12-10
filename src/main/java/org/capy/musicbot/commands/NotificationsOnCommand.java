8d0639ac2663a36e14fdf1b1abec7d8bfb3cc60e
purrfessor <nvkreskin@gmail.com>
2017-12-10 19:05:54 +0300

    added notifications commands

---
+++ src/main/java/org/capy/musicbot/commands/NotificationsOnCommand.java
@@ -0,0 +1,38 @@
+package org.capy.musicbot.commands;
+
+import org.capy.musicbot.database.MongoManager;
+import org.capy.musicbot.entities.User;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.bots.AbsSender;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * Created by enableee on 10.12.17.
+ */
+public class NotificationsOnCommand extends BotCommand {
+    public NotificationsOnCommand() {
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user) {
+        long id = user.getId();
+        MongoManager mongoManager = MongoManager.getInstance();
+        StringBuilder messageBuilder = new StringBuilder();
+
+        mongoManager.addUser(user);
+        if (mongoManager.findUser(id) != null) {
+            mongoManager.setUserNotificationsMode(id, true);
+            messageBuilder.append("I successfully turned notifications mode on!");
+        } else {
+            messageBuilder.append("Oops! Something went wrong. Please, try again.");
+        }
+        SendMessage message = new SendMessage() // Create a message object object
+                .setChatId(user.getChatId())
+                .setText(messageBuilder.toString());
+        try {
+            absSender.execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
