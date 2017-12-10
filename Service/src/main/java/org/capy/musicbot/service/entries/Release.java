e4970c52de25e46f00840ebb7336ffbab6fff98f
BlizzedRu <me@blizzed.ru>
2017-12-10 18:51:58 +0300

    Release entry complemented

--- Service/src/main/java/org/capy/musicbot/service/entries/Release.java
+++ Service/src/main/java/org/capy/musicbot/service/entries/Release.java
@@ -1,15 +1,71 @@
 package org.capy.musicbot.service.entries;
 
+import java.time.Instant;
+
 public class Release {
 
-    public enum Type {
-        ALBUM, TRACK
-    }
+    private String title;
 
     private Artist artist;
-
     private long id;
+    private Instant date;
+    private Type type;
+    private String image;
 
+    public Release(Artist artist, long id) {
+        this.artist = artist;
+        this.id = id;
+    }
 
+    public Artist getArtist() {
+        return artist;
+    }
 
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public enum Type {
+        ALBUM, SINGLE, EP
+    }
 }
