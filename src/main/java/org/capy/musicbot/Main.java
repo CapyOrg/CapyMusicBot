280567fd08eb181d52935bac6c767085d6e1182e
BlizzedRu <me@blizzed.ru>
2017-12-15 14:32:31 +0300

    Handled null location in EventsUpdater

--- src/main/java/org/capy/musicbot/Main.java
+++ src/main/java/org/capy/musicbot/Main.java
@@ -1,12 +1,19 @@
 package org.capy.musicbot;
 
 import org.capy.musicbot.database.DBConfig;
+import org.capy.musicbot.entities.Artist;
+import org.capy.musicbot.notifier.Notifier;
 import org.capy.musicbot.service.ServiceException;
+import org.capy.musicbot.service.entries.Event;
+import org.capy.musicbot.service.entries.Release;
+import org.capy.musicbot.updater.UpdateService;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by enableee on 09.12.17.
@@ -14,7 +21,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws ServiceException {
-        ApiContextInitializer.init();
+        /*ApiContextInitializer.init();
         TelegramBotsApi botsApi = new TelegramBotsApi();
         //load properties to get bot token and username
         try {
@@ -29,5 +36,22 @@ public class Main {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+*/
+        UpdateService.with(new UpdateService.DataProvider() {
+            @Override
+            public List<Artist> getArtists() {
+                return Collections.emptyList();
+            }
+        }).startTracking(new Notifier() {
+            @Override
+            public void notifyReleases(Artist artist, List<Release> releases) {
+
+            }
+
+            @Override
+            public void notifyEvents(Artist artist, List<Event> events) {
+
+            }
+        });
     }
 }
